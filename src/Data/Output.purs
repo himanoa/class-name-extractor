935925998c50e824774ff46feddbf243cafb9116
himanoa <matsunoappy@gmail.com>
2024-11-30 16:56:51 +0900

    Fix smart constructors to use fixed CSS paths and configurable filenames
    The change improves the interface usability by separating concerns:
    
    - Smart constructors now use a fixed CSS path internally, while allowing
      filename customization through parameters
    - Users can now create Output instances by only specifying the target
      filename, as long as they know the CSS path
    - This separation makes the interface more intuitive by removing the need
      to handle complete output paths when only filename changes are needed
    - Implementation details of path construction are now encapsulated within
      the constructors themselves
    
    The main benefit is that users can focus on naming their output files
    while relying on predefined CSS paths, leading to a more straightforward
    and less error-prone API.

--- src/Data/Output.purs
+++ src/Data/Output.purs
@@ -6,8 +6,12 @@ import ClassNameExtractor.CssParser (SelectorF)
 import Data.Eq.Generic (genericEq)
 import Data.Generic.Rep (class Generic)
 import Data.List (List)
+import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
-import Node.Path (FilePath)
+import Data.String (Pattern(..), Replacement(..), replace, toUpper)
+import Data.String.CodeUnits (singleton, uncons)
+import Node.Path (FilePath, basename, dirname, extname)
+import Node.Path as Path
 
 newtype FileBody = FileBody String
 derive instance Generic FileBody _
@@ -46,6 +50,38 @@ instance Show Output where
 instance Eq Output where
   eq = genericEq
 
+-- | Replacement file extension
+-- | ```purescript
+-- | > replaceExt foo.js "css"
+-- | foo.css
+-- | ```
+replaceExt :: String -> String -> String
+replaceExt path newExt = 
+  let
+    dir = dirname path
+    base = basename path
+    ext = extname path
+    baseWithoutExt = replace (Pattern ext) (Replacement "") base
+    newBase = baseWithoutExt <> "." <> newExt
+  in
+    Path.concat [dir, newBase]
+
+-- | Filename head to upper
+-- | ```purescript
+-- | > capitalizeFilename foo.js
+-- | Foo.js
+-- | ```
+capitalizeFilename :: String -> String
+capitalizeFilename path =
+  let
+    dir = dirname path
+    base = basename path
+    capitalized = case uncons base of
+      Nothing -> base
+      Just { head, tail } -> toUpper (singleton head) <> tail
+  in
+    Path.concat [dir, capitalized]
+
 -- | Make a CSS file output
 -- | ```purescript run
 -- | > makeCssFile (Namespace "Data.Foo.Bar") ".foo { display: flex }"
@@ -57,17 +93,17 @@ makeCssFile ns fb =  CssFile { namespace: ns, body: fb }
 
 -- | Make a Js file output
 -- | ```purescript run
--- | > makeJsFile "./src/components/styles.js"
--- | JsFile { path ::  "./src/components/styles.js" } 
+-- | > makeJsFile "./src/components/styles.css"
+-- | JsFile { path ::  "./src/components/Styles.js" } 
 -- | ```
 makeJsFile :: FilePath -> Output
-makeJsFile path =  JsFile { path }
+makeJsFile path = JsFile { path: capitalizeFilename $ replaceExt path "js"  }
 
 
 -- | Make a Purs file output
 -- | ```purescript run
--- | > makePursFile (Namespace "Data.Foo.Bar") "./src/components/styles.js" List.fromFoldable [Class "foo"]
+-- | > makePursFile (Namespace "Data.Foo.Bar") "./src/components/styles.css" List.fromFoldable [Class "foo"]
 -- | PursFile { path ::  "./src/components/Style.purs", namespace: (Namespace "Data.Foo.Bar"), classNames: [Class "foo"] } 
 -- | ```
 makePursFile :: Namespace -> FilePath -> List SelectorF  -> Output
-makePursFile namespace path classNames =  PursFile { path, namespace, classNames }
+makePursFile namespace path classNames =  PursFile { path: capitalizeFilename $ replaceExt path "purs", namespace, classNames }
