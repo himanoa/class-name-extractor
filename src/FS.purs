c4a811efd81e6742ebc3ee5ed1f3c8dc8d953060
himanoa <matsunoappy@gmail.com>
2024-12-01 11:20:32 +0900

    Fix a bug where writeFile failed when output directory not found.

--- src/FS.purs
+++ src/FS.purs
@@ -8,7 +8,8 @@ import Data.Maybe (maybe)
 import Effect.Aff.Class (liftAff)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff as FS
-import Node.Path (FilePath)
+import Node.FS.Perms (permsReadWrite)
+import Node.Path (FilePath, dirname)
 
 class Monad m <= MonadFS m where
   readFile :: FilePath -> m String
@@ -19,6 +20,7 @@ instance monadFSRIO :: MonadFS RIO where
   readFile path = do
     liftAff $ FS.readTextFile UTF8 path
   writeFile path body = do
+    void $ liftAff $ FS.mkdir' (dirname path) { mode: permsReadWrite, recursive: true } 
     liftAff $ FS.writeTextFile UTF8 path (coerceFileBody body)
   isExists path = do
     maybeErr <- liftAff $ FS.access path
